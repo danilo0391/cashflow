e654a92e58c577c9acb617963e5432b104b93c66
Danilo_Pereira <danilo_tw@hotmail.com>
2021-04-21 11:39:13 +0100

    Frontend Income Implementation

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,8 +1,46 @@
-import "./App.css";
-import ListUsersComponent from "./ListUsersComponent";
+import React from "react";
 
-function App() {
-	return <ListUsersComponent />;
+import { Container, Row, Col } from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import NavigationBar from "./components/NavigationBar";
+import Welcome from "./components/Welcome";
+import Footer from "./components/Footer";
+import User from "./components/User";
+import Income from "./components/Income";
+import IncomeList from "./components/IncomeList";
+import UserList from "./components/UserList";
+
+export default function App() {
+	const marginTop = {
+		marginTop: "20px",
+	};
+
+	const heading = "Welcome to Cash Flow App";
+	const desc =
+		"In this application you gonna be able to add, delete, update, and read data from the database.";
+
+	return (
+		<Router>
+			<NavigationBar />
+			<Container>
+				<Row>
+					<Col lg={12} style={marginTop}>
+						<Switch>
+							<Route
+								path="/"
+								exact
+								component={() => <Welcome heading={heading} desc={desc} />}
+							/>
+							<Route path="/add" exact component={Income} />
+							<Route path="/edit/:id" exact component={Income} />
+							<Route path="/list" exact component={IncomeList} />
+							<Route path="/users" exact component={UserList} />
+						</Switch>
+					</Col>
+				</Row>
+			</Container>
+			<Footer />
+		</Router>
+	);
 }
-
-export default App;
